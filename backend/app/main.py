ca71837a7c32e7ad4057524c79b3bcdf2f4bcae5
hoanghai4804 <hai.nguyenvuhoang4804@gmail.com>
2026-02-24 15:14:53 +0000

    Add frontend Dockerfile and Nginx

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,11 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost",      # Nginx
+        "http://localhost:80",
+        "http://localhost:3000", # Dev
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
